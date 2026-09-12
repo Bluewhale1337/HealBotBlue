50a64c5549e4d0896bf2039141e119db3824bcfb
Bluewhale1337 <295648290+Bluewhale1337@users.noreply.github.com>
2026-09-12 12:25:49 +0200

    fix: return nil instead of index when excluding forms from unshifting

--- HealBot_Controller_Spells.lua
+++ HealBot_Controller_Spells.lua
@@ -873,7 +873,7 @@ function HealBot_GetShapeshiftForm()
           local icon_lower = string.lower(icon)
           -- Exclude non-standard/custom forms like Tree of Life from unshifting
           if not string.find(icon_lower, "humanoidform") and not string.find(icon_lower, "treeoflife") and not string.find(icon_lower, "healingway") and not string.find(icon_lower, "stoneclawtotem") then
-            return i;
+            return nil;
           end
         else
           return i;
